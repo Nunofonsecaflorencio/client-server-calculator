3d2fa2e6209c8feba49e71d657866cba65967d99
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-08-31 10:23:39 +0200

    feat(server): returns result to client GUI

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
+import socket
 from client import CalculatorGUI
+from server import send_request, connect_to_server
 
-def connect_to_server(host, port): 
-    # TODO: returns a socket
-    pass
-
-def send_request(expression):
-    # TODO: Send the expression to the server, wait for server response and returns the result
-    return eval(expression) # temporary
 
 if __name__ == '__main__':
-    socket = connect_to_server('localhost', 5000)
-    
+    client_socket = connect_to_server('localhost', 5000)
+
     client = CalculatorGUI()
-    client.calculate = send_request
+    client.calculate = lambda expr: send_request(expr, client_socket)
     client.mainloop()
-    
-    # socket.close()
+
+    client_socket.close()
--- server.py
+++ server.py
@@ -1,1 +1,56 @@
-# TODO: Implement the server that receives an expression and returns the answer. (eg. request='4+2', response=6)
+import socket
+
+
+def start_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print(f"Server started, listening on {host}:{port}")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f'Connection from {addr}')
+
+        # Receive and process data
+        try:
+            while True:
+                data = client_socket.recv(1024)
+                if data:
+                    expression = data.decode('utf-8')
+                    print(f"Received expression: {expression}")
+
+                    try:
+                        result = str(eval(expression))
+                    except Exception as e:
+                        result = 'ERROR'
+
+                    client_socket.sendall(result.encode('utf-8'))
+                else:
+                    break
+        except Exception as e:
+            print(f'Error: {e}')
+        finally:
+            client_socket.close()
+            print(f"Connection closed from {addr}")
+
+
+def connect_to_server(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f"Connected to server at {host}:{port}")
+
+    return client_socket
+
+
+def send_request(expression, client_socket):
+    print(f"Sending expression to server: {expression}")
+    client_socket.sendall(expression.encode('utf-8'))
+
+    result = client_socket.recv(1024)
+    print(f"Received result from server: {result.decode('utf-8')}")
+
+    return result.decode('utf-8')
+
+
+if __name__ == '__main__':
+    start_server('localhost', 5000)
